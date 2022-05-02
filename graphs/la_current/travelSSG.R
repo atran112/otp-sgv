f9e6e628caa62df2d120b8ade0e485eb642dd9cd
Andre Tran <53277408+atran112@users.noreply.github.com>
2022-05-02 13:46:01 -0700

    Add files via upload

---
+++ graphs/la_current/travelSSG.R
@@ -0,0 +1,27 @@
+# Accompanying code for Part 2 of OTP Tutorial
+
+library(httr)
+
+travelArea <- GET(
+  "http://localhost:8080/otp/routers/la_current/isochrone",
+  query = list(
+    fromPlace = "34.048322, -118.098247", # latlong of South San Gabriel
+    arriveBy = FALSE,
+    mode = "WALK, TRANSIT", # modes we want the route planner to use
+    date = "04-04-2022",
+    time = "08:00am",
+    maxWalkDistance = 1600, # in metres
+    walkReluctance = 5,
+    minTransferTime = 600, # in secs (allow 10 minutes)
+    cutoffSec = 900,
+    cutoffSec = 1800,
+    cutoffSec = 2700,
+    cutoffSec = 3600
+  )
+)
+
+# convert airport_current to text
+travelArea <- content(travelArea, as = "text", encoding = "UTF-8")
+
+# change file path if required
+write(travelArea, file = "travelSSG.geojson")
